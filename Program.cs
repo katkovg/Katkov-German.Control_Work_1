2e81af1bb2b9ba0c841528f5df80b75ec2b5659d
Katkov German <katkov.german@yandex.ru>
2022-11-23 17:39:58 +0300

    Added inialization of array, user input & method ShowArray

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿void ShowArray (string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+int size = 0;
+Console.Write("Введите число элементов в массиве: ");
+size = Convert.ToInt32(Console.ReadLine());
+while(size < 1)
+{
+    Console.Write("Число элементов в массиве должно быть больше нуля. Введите число элементов в массиве ещё раз: ");
+    size = Convert.ToInt32(Console.ReadLine());
+}
+
+string[] arrayWords = new string [size];
